f5d1f9de16bbe126d251cf97e8003e3da586351e
JunWang <1103870790@qq.com>
2018-10-03 00:38:23 +0800

    add nav

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,31 +22,122 @@
 </head>
 
 <body>
-    <v-toolbar id="toolbar" dark color="primary">
-        <v-toolbar-side-icon></v-toolbar-side-icon>
-        <v-toolbar-title class="white--text">prer</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn icon>
-            <v-icon>search</v-icon>
-        </v-btn>
-        <v-btn icon>
-            <v-icon>apps</v-icon>
-        </v-btn>
-        <v-btn icon>
-            <v-icon>refresh</v-icon>
-        </v-btn>
-        <v-btn icon>
-            <v-icon>more_vert</v-icon>
-        </v-btn>
-    </v-toolbar>
+    <v-app id="inspire">
+        <v-navigation-drawer v-model="drawer" fixed clipped app>
+            <v-list dense>
+                <v-list-tile v-for="item in items" :key="item.text" @click="">
+                    <v-list-tile-action>
+                        <v-icon>@{{ item.icon }}</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title>
+                            @{{ item.text }}
+                        </v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+                <v-subheader class="mt-3 grey--text text--darken-1">SUBSCRIPTIONS</v-subheader>
+                <v-list>
+                    <v-list-tile v-for="item in items2" :key="item.text" avatar @click="">
+                        <v-list-tile-avatar>
+                            <img :src="`https://randomuser.me/api/portraits/men/${item.picture}.jpg`" alt="">
+                        </v-list-tile-avatar>
+                        <v-list-tile-title v-text="item.text"></v-list-tile-title>
+                    </v-list-tile>
+                </v-list>
+                <v-list-tile class="mt-3" @click="">
+                    <v-list-tile-action>
+                        <v-icon color="grey darken-1">add_circle_outline</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-title class="grey--text text--darken-1">Browse Channels</v-list-tile-title>
+                </v-list-tile>
+                <v-list-tile @click="">
+                    <v-list-tile-action>
+                        <v-icon color="grey darken-1">settings</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-title class="grey--text text--darken-1">Manage Subscriptions</v-list-tile-title>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar dark dense fixed clipped-left app>
+            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+            <v-icon class="mx-3">fab fa-youtube</v-icon>
+            <v-toolbar-title class="mr-5 align-center">
+                <span class="title">Prer</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-layout row align-center style="max-width: 350px">
+                <v-text-field :append-icon-cb="() => {}" placeholder="Search..." single-line append-icon="search" color="white"
+                    hide-details></v-text-field>
+            </v-layout>
+            <v-layout row align-center style="max-width: 50px">
+                <v-menu offset-y>
+                    <v-btn fab small slot="activator">
+                        <v-icon>person</v-icon>
+                    </v-btn>
+                    <v-list>
+                        <v-list-tile v-for="(item, index) in avatar_items" :key="index" @click="">
+                            <v-list-tile-title>@{{ item.title }}</v-list-tile-title>
+                        </v-list-tile>
+                    </v-list>
+                </v-menu>
+            </v-layout>
 
+        </v-toolbar>
+        <v-content>
+            <v-container fill-height>
+                <v-layout justify-center align-center>
+                    <v-flex shrink>
+                        <v-tooltip right>
+                            <v-btn slot="activator" :href="source" icon large target="_blank">
+                                <v-icon large>code</v-icon>
+                            </v-btn>
+                            <span>Source</span>
+                        </v-tooltip>
+                        <v-tooltip right>
+                            <v-btn slot="activator" icon large href="https://codepen.io/johnjleider/pen/YeRKwQ" target="_blank">
+                                <v-icon large>mdi-codepen</v-icon>
+                            </v-btn>
+                            <span>Codepen</span>
+                        </v-tooltip>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-content>
+    </v-app>
 
-    <script type="text/javascript">
+    <script>
         new Vue({
-            el: '#toolbar',
+            el: '#inspire',
             data: {
-                activeIndex: '1',
-                activeIndex2: '1'
+                drawer: true,
+                items: [
+                    { icon: 'trending_up', text: 'Most Popular' },
+                    { icon: 'subscriptions', text: 'Subscriptions' },
+                    { icon: 'history', text: 'History' },
+                    { icon: 'featured_play_list', text: 'Playlists' },
+                    { icon: 'watch_later', text: 'Watch Later' }
+                ],
+                items2: [
+                    { picture: 28, text: 'Joseph' },
+                    { picture: 38, text: 'Apple' },
+                    { picture: 48, text: 'Xbox Ahoy' },
+                    { picture: 58, text: 'Nokia' },
+                    { picture: 78, text: 'MKBHD' }
+                ],
+                avatar_items: [
+                    { title: 'Click Me' },
+                    { title: 'Click Me' },
+                    { title: 'Click Me' },
+                    { title: 'Click Me 2' }
+                ]
+            },
+            methods: {
+                reverseMessage: function () {
+                    this.message = this.message.split('').reverse().join('')
+                }
+            },
+            props: {
+                source: String
             }
         })
     </script>
