9fa3ed3c4274389bd54be33717400ebc59493b49
JunWang <1103870790@qq.com>
2018-10-03 23:43:51 +0800

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -92,7 +92,7 @@
                 </v-menu>
             </v-layout>
             <v-layout row align-center style="max-width: 100px">
-                <v-dialog v-model="dialog" persistent max-width="500px">
+                <v-dialog v-model="dialog" max-width="500px">
                     <v-btn slot="activator" color="primary" dark>Login</v-btn>
                     <v-card>
                         <v-card-title>
@@ -101,37 +101,18 @@
                         <v-card-text>
                             <v-container grid-list-md>
                                 <v-layout wrap>
-                                    <v-flex xs12 sm6 md4>
-                                        <v-text-field label="Legal first name" required></v-text-field>
-                                    </v-flex>
-                                    <v-flex xs12 sm6 md4>
-                                        <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
-                                    </v-flex>
-                                    <v-flex xs12 sm6 md4>
-                                        <v-text-field label="Legal last name" hint="example of persistent helper text"
-                                            persistent-hint required></v-text-field>
-                                    </v-flex>
                                     <v-flex xs12>
                                         <v-text-field label="Email" required></v-text-field>
                                     </v-flex>
                                     <v-flex xs12>
                                         <v-text-field label="Password" type="password" required></v-text-field>
                                     </v-flex>
-                                    <v-flex xs12 sm6>
-                                        <v-select :items="['0-17', '18-29', '30-54', '54+']" label="Age" required></v-select>
-                                    </v-flex>
-                                    <v-flex xs12 sm6>
-                                        <v-autocomplete :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
-                                            label="Interests" multiple chips></v-autocomplete>
-                                    </v-flex>
                                 </v-layout>
                             </v-container>
-                            <small>*indicates required field</small>
                         </v-card-text>
                         <v-card-actions>
                             <v-spacer></v-spacer>
-                            <v-btn color="blue darken-1" flat @click.native="dialog = false">Close</v-btn>
-                            <v-btn color="blue darken-1" flat @click.native="dialog = false">Save</v-btn>
+                            <v-btn color="blue darken-1" flat @click.native="dialog = false">Login</v-btn>
                         </v-card-actions>
                     </v-card>
                 </v-dialog>
