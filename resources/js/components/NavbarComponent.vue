3a7eedcd9ba4c846c7035c55685ff1d49f7d63b1
JunWang <1103870790@qq.com>
2018-10-07 14:21:46 +0800

    home link

--- resources/js/components/NavbarComponent.vue
+++ resources/js/components/NavbarComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-        <h5 class="my-0 mr-md-auto font-weight-normal">Prer</h5>
+        <router-link to="/" class="my-0 mr-md-auto font-weight-normal">Prer</router-link>
         <nav class="my-2 my-md-0 mr-md-3">
             <!--
             <a class="p-2 text-dark" href="#">Features</a>
